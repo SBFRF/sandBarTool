9976a749c32d8384c1b7e3774df581ea2d6fd7c2
Spicer Bak <spicer.bak.frf@gmail.com>
2020-04-27 09:45:48 -0400

    updated to load and use cresentic sandbar data sent by chuan, need to add geomorphon capability for sandbar tracking

--- testOnCresenticBar.py
+++ testOnCresenticBar.py
@@ -8,27 +8,28 @@ with open(fname, 'rb') as fid:
     data = pickle.load(fid)
     
 xFRF = np.arange(data.shape[1])
-
-
-plt.figure()
-ax1 = plt.subplot(211)
-mesh = ax1.pcolormesh(data)
-plt.colorbar(mesh)
-for i in range(data.shape[0]):
-    profile = -data[i]
+plt.ioff()
+for ii in range(0, data.shape[0], 20): # in y
+    plt.figure()
+    ax1 = plt.subplot(211)
+    mesh = ax1.pcolormesh(data)
+    plt.colorbar(mesh)
+    for i in range(data.shape[0]):
+        profile = -data[i]
+        bar, trough = mL.findSandBarAndTrough1D(xFRF, profile, plotFname='test.png') #, profileTrend=np.mean(data,axis=0))
+        for bb in bar:
+            ax1.plot(xFRF[bb.astype(int)], i, 'b.')
+        for tt in trough:
+            ax1.plot(xFRF[tt.astype(int)], i, 'g.')
+    
+    ax2 = plt.subplot(212)
+    profile = -data[ii]
     bar, trough = mL.findSandBarAndTrough1D(xFRF, profile, plotFname='test.png') #, profileTrend=np.mean(data,axis=0))
+    ax2.plot(xFRF, -data[ii], label='{}'.format(ii))
     for bb in bar:
-        ax1.plot(xFRF[bb.astype(int)], i, 'b.')
+        ax2.plot(xFRF[bb.astype(int)], profile[bb.astype(int)], 'b.')
     for tt in trough:
-        ax1.plot(xFRF[tt.astype(int)], i, 'g.')
-
-ax2 = plt.subplot(212)
-for i in [1240, 1260, 1280, 1300]: # in y
-    profile = -data[i]
-    bar, trough = mL.findSandBarAndTrough1D(xFRF, profile, plotFname='test.png') #, profileTrend=np.mean(data,axis=0))
-    ax2.plot(xFRF, -data[i], label='{}'.format(i))
-    for bb in bar:
-        ax1.plot(xFRF[bb.astype(int)], profile[bb.astype(int)], 'b.')
-    for tt in trough:
-        ax1.plot(xFRF[tt.astype(int)], profile[bb.astype(int)], 'g.')
+        ax2.plot(xFRF[tt.astype(int)], profile[bb.astype(int)], 'g.')
     ax2.legend()
+    plt.savefig("cresenticBarPlots/profile_{:0004}.png".format(ii))
+    plt.close()
